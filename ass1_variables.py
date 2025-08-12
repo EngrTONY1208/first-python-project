7b5c0d2c2ba6e2ee0e3e1ea4d9012c9be1037681
EngrTONY1208 <117730371+EngrTONY1208@users.noreply.github.com>
2025-08-12 18:32:40 +0100

    Add files via upload

---
+++ ass1_variables.py
@@ -0,0 +1,8 @@
+name=input("What is yur name?: ")
+print(name)
+age=input("How old are you?: ")
+print(age)
+Programming_language= input("whats your favorite programming language?: ")
+print("My name is "  + name   + " I'm "  +age  + " years old. My favorite programming language is " + Programming_language  )
+
+print("Hello " + name  +"! see you are " +age+ " years old and love " + Programming_language + ".That's a great choice!")
